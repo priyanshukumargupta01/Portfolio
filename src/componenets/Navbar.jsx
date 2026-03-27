f9736362bf7174d11474fd59c9fa8dbf688041f1
priyanshukumargupta01 <priyanshukum1368@gmail.com>
2026-03-27 23:58:16 +0530

    Updated

--- src/componenets/Navbar.jsx
+++ src/componenets/Navbar.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import OverlayMenu from "./OverlayMenu"
-
+import Logo from "../assets/Logo.png"
 
 export default function Navbar(){
     const [menuOpen , setMenuOpen]= useState(false);
@@ -8,7 +8,12 @@ export default function Navbar(){
 
     return(
       <>
-      <nav className={`fixed top-0 left-0 `}> 
+      <nav className={`fixed top-0 left-0 w-full flex items-center justify-between px-6 py-4 z-50 transition-transform duration-300 ${visible ? "translate-y-o" : -translate-y-full}`}> 
+
+        <div className="flex gap-2 items-center">
+          <img src={Logo} alt="logo" className="w-18 h-18"/>
+          <div className="text-2xl">Priyanshu Gupta</div>
+        </div>
       </nav>
       <OverlayMenu />
       </>
