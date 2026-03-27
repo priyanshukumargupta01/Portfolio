c06a1c756eea586250316ac26d7b4a31da49c299
priyanshukumargupta01 <priyanshukum1368@gmail.com>
2026-03-28 00:34:08 +0530

    Updated

--- src/componenets/Navbar.jsx
+++ src/componenets/Navbar.jsx
@@ -1,22 +1,34 @@
 import { useState } from "react";
-import OverlayMenu from "./OverlayMenu"
-import Logo from "../assets/Logo.png"
+import OverlayMenu from "./OverlayMenu";
+import Logo from "../assets/Logo.png";
+import { FiMenu } from "react-icons/fi";
 
-export default function Navbar(){
-    const [menuOpen , setMenuOpen]= useState(false);
-    const [visible, setVisible] = useState(true)
+export default function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [visible, setVisible] = useState(true);
 
-    return(
-      <>
-      <nav className={`fixed top-0 left-0 w-full flex items-center justify-between px-6 py-4 z-50 transition-transform duration-300 ${visible ? "translate-y-o" : -translate-y-full}`}> 
-
-        <div className="flex gap-2 items-center">
-          <img src={Logo} alt="logo" className="w-18 h-18"/>
-          <div className="text-2xl">Priyanshu Gupta</div>
+  return (
+    <>
+      <nav
+        className={`fixed top-0 left-0 w-full flex items-center justify-between px-6 py-4 z-50 transition-transform duration-300 ${visible ? "translate-y-o" : -translate - y - full}`}
+      >
+        <div className="flex gap-2 items-center ">
+          <img src={Logo} alt="logo" className="w-18 h-18" />
+          <div className="text-2xl font-bold hidden sm:block">
+            Priyanshu Gupta
+          </div>
+        </div>
+        <div className="block lg:absolute right-0 lg:transform-x-1/2 mr-15 text-3xl focus:outline-none">
+          <button
+            onClick={() => {
+            setMenuOpen(true);
+            }}
+          >
+            <FiMenu />
+          </button>
         </div>
       </nav>
-      <OverlayMenu />
-      </>
-    )
-  
+      <OverlayMenu isopen={menuOpen} onclose={() => setMenuOpen(false)} />
+    </>
+  );
 }
