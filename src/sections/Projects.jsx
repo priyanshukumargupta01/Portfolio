900f8e14cc7a2deb80c236e49766a60d613d9d38
priyanshukumargupta01 <priyanshukum1368@gmail.com>
2026-04-12 23:42:52 +0530

    Skill Section

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Projects = () => {
   return (
-    <div className="h-screen w-full">Projects</div>
+    <div>Projects</div>
   )
 }
 
--- src/sections/Skills.jsx
+++ src/sections/Skills.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+  
+  const SkillsInfo = [
+  {
+    title: "Frontend",
+    skills: [   ],
+  }, 
+  {
+    title: "Tools",
+    skills: [   ],
+  },
+];
 
-const Skills = () => {
-  return (
-    <div className="h-screen w-full">Skills</div>
-  )
-}
+const Skills = () => (
+  <section
+    id="skills"
+    className="py-24 pb-24 px-[12vw] h-screen md:px-[7vw] lg:px-[20vw] font-sans bg-skills-gradient clip-path-custom"
+  > 
+    <div className="text-center mb-8">
+      <h2 className="text-3xl sm:text-4xl font-bold text-white">SKILLS</h2>
+      <div className="w-24 h-1 bg-[#ffffff] mx-auto mt-2"></div>
+      <p className="text-gray-400 mt-4 text-lg font-semibold">
+      A collection of my technical skills and expertise honed through various projects and experiences
+      </p>
+    </div>
+ 
+   
+  </section>
+);
 
-export default Skills
+export default Skills;
