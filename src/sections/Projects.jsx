acfcc19deb542208798c266fe99d6d5fea169709
priyanshukumargupta01 <priyanshukum1368@gmail.com>
2026-04-26 23:13:06 +0530

    Project Section

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Projects = () => {
   return (
-    <div>Projects</div>
+    <section
+      className='min-h-screen bg-black w-full'
+      id='project'
+    >
+      <div>
+        <h2 className='text-4xl font-bold mb-4 justify-center flex'>My Projects</h2>
+        <div className='w-32 h-1 bg-white mx-auto mt-4'></div>
+        <p className='text-gray-400 mt-4 text-lg font-semibold flex justify-center'>Here are some of my recent projects:</p>
+      </div>
+
+    </section>
   )
 }
 
