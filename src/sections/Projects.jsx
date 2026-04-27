160beb179caa2bb618bcc7d18dcb07eebb683f7e
priyanshukumargupta01 <priyanshukum1368@gmail.com>
2026-04-27 11:48:12 +0530

    Updated

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -1,4 +1,4 @@
-import { i, img } from 'framer-motion/client';
+import { motion } from "framer-motion";
 import React from 'react'
 import logo from '../assets/logo.png';
 import Car from '../assets/car.jpg';
@@ -10,22 +10,23 @@ import Car from '../assets/car.jpg';
 //   "top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[220px] h-[220px] opacity-10 blur-[100px]",
 // ];
 
+
 const projects = [
   {
-    id : 0,
+    id: 0,
     title: "SmartPark",
-    description: "SmartParking is an online parking management system for malls that allows users to pre-book parking slots in advance. It helps reduce waiting time, manage parking space efficiently, and improve the overall user experience.", 
-    img: logo, 
-    tags: ["React","HTML", "CSS", "JavaScript", "Node.js", "Express", "MongoDB" ,"tailwind", "framer-motion"],
+    description: "SmartParking is an online parking management system for malls that allows users to pre-book parking slots in advance. It helps reduce waiting time, manage parking space efficiently, and improve the overall user experience.",
+    img: Car,
+    tags: ["React", "HTML", "CSS", "JavaScript", "Node.js", "Express", "MongoDB", "tailwind", "framer-motion"],
     github: "https://github.com/user/project-one",
     web: "https://example.com/project-one"
   },
   {
-    id : 1,
-    title: "Project Two",
-    description: "My portfolio is my personal website where I showcase my projects, skills, and basic information about myself. It is designed to be simple, responsive, and easy to use, so anyone can quickly understand my work and experience.", 
-    img: Car,
-    tags: ["React","HTML", "CSS", "JavaScript", "tailwind", "framer-motion"],
+    id: 1,
+    title: "Portfolio",
+    description: "My portfolio is my personal website where I showcase my projects, skills, and basic information about myself. It is designed to be simple, responsive, and easy to use, so anyone can quickly understand my work and experience.",
+    img: logo,
+    tags: ["React", "HTML", "CSS", "JavaScript", "tailwind", "framer-motion"],
     github: "https://github.com/user/project-two",
     web: "https://example.com/project-two"
   }
@@ -36,7 +37,7 @@ const Projects = () => {
     <section
       className='min-h-screen bg-black w-full'
       id='project'
-    >   
+    >
       {/* <div className="absolute inset-0 pointer-events-none">
         {glows.map((c, i) => (
           <div
@@ -45,11 +46,38 @@ const Projects = () => {
           />
         ))}
       </div> */}
-      <div>
+      <div className='text-center mb-16 '>
         <h2 className='text-4xl font-bold mb-4 justify-center flex'>My Projects</h2>
         <div className='w-32 h-1 bg-white mx-auto mt-4'></div>
         <p className='text-gray-400 mt-4 text-lg font-semibold flex justify-center'>Here are some of my recent projects:</p>
-      </div> 
+      </div>
+      <div
+        className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3'
+      >
+        {projects.map((project) => (
+          <motion.div
+            initial={{ opacity: 0, y: 24 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.6 }}
+            viewport={{ once: true, amount: 0.4 }}
+            key={project.id} className='bg-gray-950 rounded-lg overflow-hidden shadow-lg m-4'>
+            <img src={project.img} alt={project.title} className='w-full h-56 object-cover' />
+            <div className='p-4'>
+              <h3 className='text-xl font-bold mb-2'>{project.title}</h3>
+              <p className='text-gray-400 mb-4'>{project.description}</p>
+              <div className='flex flex-wrap gap-2'>
+                {project.tags.map((tag, index) => (
+                  <span key={index} className='bg-gray-700 text-white text-xs px-2 py-1 rounded'>
+                    {tag}
+                  </span>
+                ))}
+              </div>
+            </div>
+          </motion.div>
+        ))}
+
+
+      </div>
     </section>
   )
 }
