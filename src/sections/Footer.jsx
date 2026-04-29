28c987a2689921aa79a5781f5235e59f44eb663d
priyanshukumargupta01 <priyanshukum1368@gmail.com>
2026-04-29 22:32:23 +0530

    Footer section

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className="h-screen w-full">Footer</div>
+    <section
+    className='min-h-full w-full bg-black'
+    id='Footer'
+    >
+<div
+  className='text-center mb-16 flex justify-center  '
+  >
+    <h2
+    className='text-4xl font-bold m-2.5 '
+    > Priyanshu Kumar</h2>
+</div>
+
+    </section>
   )
 }
 
